144aab0e27031fe6cef19e3da6aed63511e4628f
Feven-Tolosa <tolosafeven0@gmail.com>
2025-08-26 00:48:41 +0300

    resume page updated mage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import About from '@/components/About'
 import Contact from '@/components/Contact'
 import Hero from '@/components/Hero'
 import Projects from '@/components/Projects'
-import Resume from '@/components/Resume'
 import Skills from '@/components/Skills'
 
 export default function Home() {
