0f1872857ed5cc10954988e6cad2dd9fc035902b
Feven-Tolosa <tolosafeven0@gmail.com>
2026-01-13 16:34:50 +0300

    resume updated

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,4 +1,3 @@
-// components/Skills.tsx
 'use client'
 
 import { useEffect, useState } from 'react'
@@ -11,7 +10,6 @@ export default function Skills() {
   }, [])
 
   const skills = [
-    // from-gray-800 to-gray-900
     {
       name: 'HTML5',
       level: 95,
