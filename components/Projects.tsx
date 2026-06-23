b475205fd0668fae9d752c87ba8f96d4e7445ab7
Feven-Tolosa <tolosafeven0@gmail.com>
2026-06-23 23:38:39 +0300

    project add

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,6 +9,14 @@ export default function Projects() {
 
   const projects = [
     {
+      title: 'Gosple Truth Ethiopia',
+      description: 'church website',
+      image: '/images/project10.png',
+      link: 'https://gospel-truth-ethiopia.vercel.app/',
+      tags: ['Next.js', 'Talwind', 'YouTube API Intgration'],
+      category: 'web',
+    },
+    {
       title: 'Selam Markets',
       description:
         'An e-commerce platform for local vendors to sell their products online and for customers to discover and purchase a local wide variety of goods',
