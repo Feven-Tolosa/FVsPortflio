05de11dcc40c213e714db1de18edeaf76e62f053
Feven-Tolosa <tolosafeven0@gmail.com>
2026-08-18 23:47:24 +0300

    fix: message api fixed

--- app/api/message/route.ts
+++ app/api/message/route.ts
@@ -20,6 +20,9 @@ export async function POST(req: Request) {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
+      tls: {
+        rejectUnauthorized: false,
+      },
     })
 
     await transporter.sendMail({
